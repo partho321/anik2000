bb2cbaa328d1710209c448af4c758da907bc6928
partho321 <anik.partho555@gmail.com>
2021-12-14 18:51:09 +0600

    Lab task 5

---
+++ Lab Task 5/bookAddedDone.php
@@ -0,0 +1,27 @@
+
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+ <?php include('header2.php')?>
+
+
+
+
+<span style="color: green"> <b> <h1 align="center"><?php echo  "Book has added successfully";?></h1> </span>
+
+<body>
+
+
+
+
+
+</body>
+<div class="footer">
+      <?php include('footer.php')?> 
+</div>
+</html>
---
+++ Lab Task 5/passwordOk.php
@@ -0,0 +1,26 @@
+
+
+
+<!DOCTYPE html>
+<html>
+
+ <?php include('header.php')?>
+
+
+
+<p><br></p><p><br></p>
+        <span > <b> <h1 align="center"><?php echo  "Password Changed Successfully  ";?></h1> </span>
+<p><br></p><p><br></p>
+
+<body>
+
+
+
+
+
+</body>
+
+
+  <?php include('footer.php')?>
+
+  </html>
